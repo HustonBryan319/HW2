80ee14a091a293c7e954e46d4721a90d641c4c16
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-11-07 16:52:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,18 @@ include "view-header.php";
 ?>
     <h1>Home</h1>
 <?php
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Soccer player</h2>
+
+<p id="demo">who is my favorite player?.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Lionel Messi"'>Click to find out!</button>
+
+</body>
+</html>
+
 include "view-footer.php";
 ?>
