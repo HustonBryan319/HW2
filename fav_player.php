432ea84f08dca90f6b9d0534245102211dc26351
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-11-07 16:55:01 -0600

    Create fav_player.php

---
+++ fav_player.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Soccer player</h2>
+
+<p id="demo">who is my favorite player?.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Lionel Messi"'>Click to find out!</button>
+
+</body>
+</html>
